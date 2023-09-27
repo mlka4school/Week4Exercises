5585bba239b4583126024ad16f82b304e0b70c9e
mlka4school <jc1109880@student.francistuttle.edu>
2023-09-27 12:50:02 -0500

    Finished all exercises (LINQ moment)

--- ExerciseOnLINQ/Program.cs
+++ ExerciseOnLINQ/Program.cs
@@ -12,16 +12,16 @@ namespace ExerciseOnLINQ
 		{
 			List<Item> itemList = new List<Item>(); // new list
 			// generate twelve items. some share colors. other share material and weight.
-			itemList.Add(new Item(35,"plain","yellow"));
+			itemList.Add(new Item(40,"plain","yellow"));
 			itemList.Add(new Item(50,"gooey","orange"));
 			itemList.Add(new Item(35,"soft","green"));
 			itemList.Add(new Item(40,"metal","yellow"));
 			itemList.Add(new Item(35,"soft","orange"));
 			itemList.Add(new Item(50,"plain","green"));
 			itemList.Add(new Item(35,"soft","pink"));
-			itemList.Add(new Item(40,"metal","white"));
+			itemList.Add(new Item(35,"metal","white"));
 			itemList.Add(new Item(35,"gooey","black"));
-			itemList.Add(new Item(35,"plain","white"));
+			itemList.Add(new Item(40,"plain","white"));
 			itemList.Add(new Item(50,"metal","black"));
 			itemList.Add(new Item(40,"metal","green"));
 
@@ -35,13 +35,22 @@ namespace ExerciseOnLINQ
 			var queryKG = from item in itemList where item.Weight == 50 select item;
 			ListList(queryKG.ToList<Item>());
 			Console.WriteLine("");
+			// next, do the query again but only sorting.
+			Console.WriteLine("Sorting all items by kg:");
+			var querySort = from item in itemList orderby item.Weight select item;
+			ListList(querySort.ToList<Item>());
+			Console.WriteLine("");
 			// try the other method. for metal items this time
-			Console.WriteLine("Finding all metal items:");
-			var queryMT = itemList.Where(item => item.Material == "metal").Select()
+			Console.WriteLine("Finding all metal items sorting by kg:");
+			var queryMT = itemList.Where(item => item.Material == "metal").OrderBy(item => item.Weight);
 			ListList(queryMT.ToList<Item>()); // ToArray CAN be done. but i do not have a function for it
 			Console.WriteLine("");
 			Console.WriteLine("Press any key to continue...");
 			Console.ReadKey();
+			/* 
+			 * as for the discussion. i prefer method syntax in general since that's just what i'm used to, method calls are simple while query calls are different.
+			 * that being said, i can tell there are reasons for both to exist.
+			 */ 
 		}
 
 		static void ListList(List<Item> list){
